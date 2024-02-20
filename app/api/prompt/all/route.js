a731a51cb1ab763da0612f250ff1f5164736ca7b
kunal1632 <kunal.dk35@gmail.com>
2024-02-20 21:09:09 +0530

    fixing database connection

--- app/api/prompt/all/route.js
+++ app/api/prompt/all/route.js
@@ -4,7 +4,6 @@ import { connectToDB } from "@utils/database";
 export const GET = async (request) => {
   try {
     await connectToDB();
-
     const prompts = await Prompt.find({}).populate("creator");
 
     return new Response(JSON.stringify(prompts), { status: 200 });
--- utils/database.js
+++ utils/database.js
@@ -4,11 +4,6 @@ let isConnected = false; //track the connection
 
 export const connectToDB = async () => {
   mongoose.set("strictQuery", true);
-  if (isConnected) {
-    console.log("mongoDb is already connected");
-    return;
-  }
-
   try {
     await mongoose.connect(process.env.MONGODB_URI, {
       dbName: "share_prompt",
