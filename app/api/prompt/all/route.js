0713e3586f74d41c4a783802b00b9f1c9b36cf13
kunal1632 <kunal.dk35@gmail.com>
2024-02-22 17:12:53 +0530

    fix all api

--- app/api/prompt/all/route.js
+++ app/api/prompt/all/route.js
@@ -4,7 +4,7 @@ import { connectToDB } from "@utils/database";
 export const GET = async (request) => {
   try {
     await connectToDB();
-    const prompts = await Prompt.find().populate("creator").exec();
+    const prompts = await Prompt.find();
 
     return new Response(JSON.stringify(prompts), { status: 200 });
   } catch (error) {
